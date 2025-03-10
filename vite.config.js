bc96f8e2c517a207d0cfd942010b9ddc45489ea1
Mavi Joar C. Crisostomo <crisostomo.mavijoar2003@gmail.com>
2025-03-10 19:37:52 +0800

    setup client side inertia

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-
+import vue from '@inertiajs/vue3'
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
